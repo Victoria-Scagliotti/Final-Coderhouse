9ba187e082f9f8aa0cdb94d518a19472ec103067
Scagliotti <victoriascagliotti@gmail.com>
2022-10-17 23:35:43 -0300

    2da entrega

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,16 @@
+/*let usuarioAutorizado = "Admin"
+let contraseniaAutorizada = "1234"
+
+let usuario = prompt("Ingrese su usuario: ");
+
+let contrasenia = prompt("Ingrese su contraseña: ");
+
+if (usuario === usuarioAutorizado && contrasenia === contraseniaAutorizada){
+    alert("Bienvenido" + usuario);*/
+
+
+//crear un código para tomar el nombre y la contraseña de una base de datos e ingrese a tu cuenta.
+
 let bienvenida = confirm("Hola fanáticos! Están listos para romper el chanchito y llevarse artículos de su shooter favorito?");
 
 if (bienvenida) {
@@ -44,4 +57,61 @@ else {
     alert("Que lástima! Esperamos que vuelvas pronto...")
 }
 
-//crear un código para tomar el nombre y la contraseña de una base de datos e ingrese a tu cuenta.
+
+//INGRESO EL CODIGO DEL ARTICULO QUE ME INTERESA
+//LA CONSOLA ME TRAE TODO EL OBJETO
+//CONFIRMO QUE ES EL QUE QUIERO
+//INGRESO CANTIDAD
+//QUIERO COMPRAR MAS?
+//CONFIRMO CBA U OTRO LUGAR (DOS CONSTANTES CON LOS COSTOS)
+//TENGO CUPON DE DTO?
+//ME TRAE EL COSTO FINAL
+
+
+
+//Array de productos
+let productos = [ropa, keycaps, mousepad, figura, cuadro, lampara];
+
+//Objetos de cada producto
+let ropa = {
+    codigo: "R",
+    agente: "Sky",
+    cantidad: 1,
+    precio: 2500
+};
+
+let keycaps = {
+    codigo: "K",
+    agente: "Sova",
+    cantidad: 5,
+    precio: 1000
+};
+
+let mousepad = {
+    codigo: "M",
+    agente: "Viper",
+    tamaño: "XL",
+    precio: 3500
+};
+
+let figura = {
+    codigo: "F",
+    agente: "Jett",
+    cantidad: 1,
+    precio: 5000
+};
+
+let cuadro = {
+    codigo: "C",
+    agente: "Reyna",
+    diseño: "triptico",
+    precio: 5500
+};
+
+let lampara = {
+    codigo: "L",
+    agente: "Breach",
+    watts: 200,
+    precio: 6000
+};
+
